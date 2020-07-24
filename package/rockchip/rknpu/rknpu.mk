12076172007e55cdffe73c2c0d84c7f50a4cba3f
advyunjin <yunjin.jiang@advantech.com.cn>
2020-07-24 16:00:26 +0800

    update to rk3399_linux_release_v2.4.1_20200717

--- package/rockchip/rknpu/rknpu.mk
+++ package/rockchip/rknpu/rknpu.mk
@@ -7,35 +7,68 @@ RKNPU_VERSION = 1.2.1
 RKNPU_SITE_METHOD = local
 RKNPU_SITE = $(TOPDIR)/../external/rknpu
 NPU_TEST_FILE = $(@D)/test
+RKNPU_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_RKNPU_PCIE),y)
 NPU_KO_FILE = galcore_rk3399pro-npu-pcie.ko
-NPU_INIT_FILE = S99NPU_init
 else ifeq ($(BR2_PACKAGE_RK3399PRO_NPU),y)
 NPU_KO_FILE = galcore_rk3399pro-npu.ko
-NPU_INIT_FILE = S99NPU_init
+else ifeq ($(BR2_PACKAGE_RK1806),y)
+NPU_KO_FILE = galcore_rk1806.ko
+else ifeq ($(BR2_PACKAGE_RV1126_RV1109),y)
+ifeq ($(BR2_PACKAGE_RKNPU_USE_MINI_DRIVER),y)
+NPU_KO_FILE = galcore_puma_tb.ko
+else
+NPU_KO_FILE = galcore_puma.ko
+endif
 else
 NPU_KO_FILE = galcore.ko
-NPU_INIT_FILE = S99NPU_init
+endif
+
+ifeq ($(BR2_arm),y)
+NPU_PLATFORM_ARCH = linux-armhf
+else
+NPU_PLATFORM_ARCH = linux-aarch64
+endif
+
+ifeq ($(BR2_PACKAGE_RV1126_RV1109),y)
+NPU_PLATFORM_ARCH = linux-armhf-puma
+endif
+
+ifeq ($(BR2_PACKAGE_RKNPU_USE_MINI_DRIVER), y)
+NPU_PLATFORM = $(NPU_PLATFORM_ARCH)-mini
+BUILD_NOT_START_RKNN_SCRIPT=y
+else
+NPU_PLATFORM = $(NPU_PLATFORM_ARCH)
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_RKNN), y)
 BUILD_PYTHON_RKNN=y
 endif
 
+define RKNPU_INSTALL_STAGING_CMDS
+    mkdir -p $(STAGING_DIR)/usr/include/rknn
+    $(INSTALL) -D -m 0644 $(@D)/rknn/include/rknn_runtime.h $(STAGING_DIR)/usr/include/rknn/rknn_runtime.h
+endef
+
 define RKNPU_INSTALL_TARGET_CMDS
-    mkdir -p $(TARGET_DIR)/usr/lib/modules/
+    mkdir -p $(TARGET_DIR)/lib/modules/
     mkdir -p $(TARGET_DIR)/usr/share/npu/
-    $(INSTALL) -D -m 0644 $(@D)/drivers/$(NPU_KO_FILE) $(TARGET_DIR)/usr/lib/modules/galcore.ko
-    $(INSTALL) -D -m 0644 $(@D)/drivers/*.so $(TARGET_DIR)/usr/lib/
-    $(INSTALL) -D -m 0644 $(@D)/drivers/*.so $(STAGING_DIR)/usr/lib/
-    $(INSTALL) -D -m 0644 $(@D)/rknn/usr/lib/*.so $(STAGING_DIR)/usr/lib/
-    $(INSTALL) -D -m 0644 $(@D)/drivers/cl_*.h $(TARGET_DIR)/usr/lib/
-    $(INSTALL) -D -m 0755 $(@D)/$(NPU_INIT_FILE) $(TARGET_DIR)/etc/init.d/S99NPU_init
-    cp -r $(@D)/rknn/usr/* $(TARGET_DIR)/usr/
+    $(INSTALL) -D -m 0644 $(@D)/drivers/npu_ko/$(NPU_KO_FILE) $(TARGET_DIR)/lib/modules/galcore.ko
+    cp -r $(@D)/drivers/common/* $(TARGET_DIR)/
+    cp -r $(@D)/drivers/common/* $(STAGING_DIR)/
+
+    if [ x${BUILD_NOT_START_RKNN_SCRIPT} != x ]; then \
+        rm $(TARGET_DIR)/etc/init.d/S60NPU_init; \
+    else \
+        rm $(TARGET_DIR)/etc/init.d/S05NPU_init; \
+    fi
+
+    cp -r $(@D)/drivers/$(NPU_PLATFORM)/* $(TARGET_DIR)/
+    cp -r $(@D)/drivers/$(NPU_PLATFORM)/* $(STAGING_DIR)/
 
     if [ -e "$(@D)/test" ]; then \
-	cp -r $(@D)/test $(TARGET_DIR)/usr/share/npu; \
+        cp -r $(@D)/test $(TARGET_DIR)/usr/share/npu; \
     fi
 
     if [ x${BUILD_PYTHON_RKNN} != x ]; then \
